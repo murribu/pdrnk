31b9e39dff94028ef8ea24995edb53056fd71bf3
Cory Martin <murribu@gmail.com>
2014-10-17 11:14:27 -0500

    ng / firebase init

---
+++ app/scripts/controllers/nav.js
@@ -0,0 +1,19 @@
+'use strict';
+
+app.controller('NavCtrl', function ($scope, $location, Post, Auth) {
+  $scope.post = {url: 'http://', title: ''};
+  $scope.user = Auth.user;
+  
+  $scope.submitPost = function () {
+    $scope.post.creator = $scope.user.profile.username;
+    $scope.post.creatorID = $scope.user.id;
+    Post.create($scope.post).then(function (ref) {
+      $location.path('/posts/' + ref.name());
+      $scope.post = {url: 'http://', title: ''};
+    });
+  };
+  
+  $scope.signedIn = Auth.signedIn;
+  $scope.logout = Auth.logout
+  
+});
