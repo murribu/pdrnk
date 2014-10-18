00d0539f891ef8a5f3813e029c9ca1218f17aff8
CORY M MARTIN (iMac) <murribu@gmail.com>
2014-10-18 10:01:22 -0500

    yo angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pdrnkApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the pdrnkApp
+ */
+angular.module('pdrnkApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
